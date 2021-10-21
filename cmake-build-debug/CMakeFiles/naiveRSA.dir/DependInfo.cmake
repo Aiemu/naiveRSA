f0b0f7802b444e6486539554b71c36c53212a364
ZengZheng <zengz17@outlook.com>
2021-10-21 17:12:35 +0800

    Update .gitignore, remove .idea

---
+++ cmake-build-debug/CMakeFiles/naiveRSA.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Aiemu/Documents/02-Coursewares/Master/Encryption/navieRSA/include/BigInteger.cpp" "/Users/Aiemu/Documents/02-Coursewares/Master/Encryption/navieRSA/cmake-build-debug/CMakeFiles/naiveRSA.dir/include/BigInteger.cpp.o"
+  "/Users/Aiemu/Documents/02-Coursewares/Master/Encryption/navieRSA/main.cpp" "/Users/Aiemu/Documents/02-Coursewares/Master/Encryption/navieRSA/cmake-build-debug/CMakeFiles/naiveRSA.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/naiveRSA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/naiveRSA.dir/include/BigInteger.cpp.o"
+  "CMakeFiles/naiveRSA.dir/main.cpp.o"
+  "naiveRSA"
+  "naiveRSA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/naiveRSA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
